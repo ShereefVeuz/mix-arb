19f6c658612345907287a2fc3e3f11c776d30368
shereef <shareef@veuz.in>
2025-05-28 20:01:27 +0530

    td2 tow commit

--- assets/js/custom-innerpage.js
+++ assets/js/custom-innerpage.js
@@ -647,7 +647,7 @@ $(document).ready(function() {
     autoplayTimeout: 2000,
     autoplayHoverPause: true,
     center: true,
-    ltr: true,
+    rtl: true,
     stagePadding: 0,
     loop: true,
     responsive: {
@@ -704,6 +704,46 @@ gsap.from(".right-page-career-in-out", {
       scrub: true,
       markers: false,
       toggleActions: "play reverse play reverse",
+  }
+});
+
+
+// change text
+$(document).ready(function () {
+  var changebox = $(".changebox");
+
+  var firstclone = changebox.children(":first").clone();
+  changebox.append(firstclone);
+
+  var fsstr = changebox.parent().css("font-size");
+  fsstr = fsstr.slice(0, fsstr.indexOf("p"));
+  var fs = parseInt(fsstr);
+
+  changebox.css("height", changebox.parent().css("line-height"));
+  ChangeSize(0);
+  setInterval(Next, 2000);
+
+  function Next() {
+    if (typeof Next.i == 'undefined') {
+      Next.i = 0;
+    }
+    Next.i++;
+    if (Next.i == changebox.children("span").length) {
+      Next.i = 1;
+      changebox.scrollTop(0);
+    }
+    changebox.animate({
+      scrollTop: (fs * Next.i) + Next.i * 5 + 3
+    }, 500);
+    setTimeout(function () {
+      ChangeSize(Next.i);
+    }, 500);
+  }
+
+  function ChangeSize(i) {
+    var word = changebox.children("span").eq(i);
+    var wordsize = word.css("width");
+    changebox.css("width", wordsize);
   }
 });
 
@@ -798,9 +838,8 @@ filterContainer.addEventListener("click", (event) => {
   }
 });
 
-
-
 // clients banner
+
 
 
 // validation
